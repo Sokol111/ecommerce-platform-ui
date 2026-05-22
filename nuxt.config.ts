4a50222974bee3ce017f04b546335b714df8c632
Sokol111 <igorsokol111@gmail.com>
2026-05-22 14:41:33 +0200

    u

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,10 +46,7 @@ export default defineNuxtConfig({
         'vue-router',
         '@vueuse/core',
         'zod',
-        '@tanstack/vue-table',
-        'reka-ui',
-        '@floating-ui/dom',
-        'vaul-vue'
+        'reka-ui'
       ]
     },
     server: {
